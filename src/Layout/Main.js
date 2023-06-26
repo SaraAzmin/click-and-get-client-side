f05e5eb43d96e990cdbadd5f79d26ac26dcda5b3
SaraAzmin <azmin.sara17@gmail.com>
2023-06-26 17:56:58 +0200

    setup router and components

---
+++ src/Layout/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menubar from "../Pages/Shared/MenuBar";
+import Footer from "../Pages/Shared/Footer";
+import MenuBar from '../Pages/Shared/MenuBar';
+
+const Main = () => {
+    return (
+        <div>
+            <MenuBar></MenuBar>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
