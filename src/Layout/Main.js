476811400f7bb2901e0b6acfc7809efeb5d7c626
SaraAzmin <azmin.sara17@gmail.com>
2023-07-04 12:44:18 +0200

    fixed routes

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Menubar from "../Pages/Shared/MenuBar";
 import Footer from "../Pages/Shared/Footer";
 import MenuBar from '../Pages/Shared/MenuBar';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <MenuBar></MenuBar>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
--- src/Pages/Shared/MenuBar.js
+++ src/Pages/Shared/MenuBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MenuBar = () => {
     return (
         <div>
-            
+            <h2>menu</h2>
         </div>
     );
 };
