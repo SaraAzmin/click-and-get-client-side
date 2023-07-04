81ca3bb7cf808b7b94c1b09b9cf0f31067562d77
SaraAzmin <azmin.sara17@gmail.com>
2023-07-05 00:16:26 +0200

    added why CLICKandGET section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Home/Banner/Banner";
+import AboutApp from "../Home/AboutApp/AboutApp";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <AboutApp></AboutApp>
     </div>
   );
 };
--- src/Pages/Shared/MenuBar.js
+++ src/Pages/Shared/MenuBar.js
@@ -24,7 +24,7 @@ const MenuBar = () => {
   );
 
   return (
-    <div className="navbar bg-sky-600 text-white flex justify-between pr-20 lg:py-3 md:py-0">
+    <div className="navbar sticky top-0 bg-sky-600 text-white flex justify-between pr-20 lg:py-3 md:py-0">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
