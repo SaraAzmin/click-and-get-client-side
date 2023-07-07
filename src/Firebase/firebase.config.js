37a9cd361a1a92025e959151a421c71fb6d80515
SaraAzmin <azmin.sara17@gmail.com>
2023-07-07 02:18:54 +0200

    login page added and integrated with firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDncl-xbBecO9ApaZH1_JFW5Db_eC3iALY",
+  authDomain: "click-and-get-74d3a.firebaseapp.com",
+  projectId: "click-and-get-74d3a",
+  storageBucket: "click-and-get-74d3a.appspot.com",
+  messagingSenderId: "344397495687",
+  appId: "1:344397495687:web:fab3224b5080b102ee6722",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
