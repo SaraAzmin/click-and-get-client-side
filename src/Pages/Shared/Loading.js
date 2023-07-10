fa27623e57e407a3958a7e2591ba28ee889abdec
SaraAzmin <azmin.sara17@gmail.com>
2023-07-10 22:16:27 +0200

    added loading component

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Loading = () => {
     return (
         <div>
-            
+            <div className="flex justify-center items-center h-screen" >
+
+                <button className="btn loading text-white bg-blue-800">loading</button>
+
+            </div>
         </div>
     );
 };
